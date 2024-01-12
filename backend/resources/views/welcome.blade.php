55ccd557582b34d4c03b948794c64cb7549a03d7
adim <apple@adimMac.local>
2024-01-12 08:55:45 +0300

    skeleton 1 | backend | frontend

---
+++ backend/resources/views/welcome.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <style>
+        body{
+            background-color: #dfe7e7;
+        }
+        .content{
+            margin:200px 36%;
+            color:#3e8d78;
+        }
+        .content h2 {
+        }
+
+    </style>
+    <title>ICTC Website</title>
+</head>
+<body>
+    <div class="content">
+        <h2>ICTC Website backend API is running...</h2>
+    </div>
+</body>
+</html>
